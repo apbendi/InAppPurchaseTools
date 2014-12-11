9f36547bd33150320149c4a0de61631f1c9d2add
Ben DiFrancesco <ben.difrancesco@gmail.com>
2014-12-11 16:06:43 -0500

    Header configuration

--- InAppPurchaseTools/InAppPurchaseTools.h
+++ InAppPurchaseTools/InAppPurchaseTools.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double InAppPurchaseToolsVersionNumber;
 FOUNDATION_EXPORT const unsigned char InAppPurchaseToolsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <InAppPurchaseTools/PublicHeader.h>
-
+#import <InAppPurchaseTools/SLKInAppProductManager.h>
 
