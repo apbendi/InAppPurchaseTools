2d540b65f3b0caad4796d64f62f7c1cfa1e40862
Ben DiFrancesco <ben.difrancesco@gmail.com>
2014-12-11 15:00:33 -0500

    Adjust public interface to silently load product dictionary in the background

--- InAppPurchaseTools/SLKInAppProductManager.h
+++ InAppPurchaseTools/SLKInAppProductManager.h
@@ -7,16 +7,22 @@
 //
 // Uses the code from the following Ray Wenderlich tutorial as a starting point
 // Strips out code related to *purchasing* of IAPs and implements a simplified
-// Singleton design
+// Singleton design for accessing IAP product info
+//
+// http://www.raywenderlich.com/21081/introduction-to-in-app-purchases-in-ios-6-tutorial
 
 #import <Foundation/Foundation.h>
 #import <StoreKit/StoreKit.h>
+
+@class SKProduct;
 
 typedef void (^RequestProductsCompletionHandler)(BOOL success, NSDictionary * products);
 
 @interface SLKInAppProductManager : NSObject
 
+@property (strong, nonatomic) NSDictionary *skProductDictionary;
+
 - (id)initWithProductIdentifiers:(NSSet *)productIdentifiers;
-- (void)requestProductsWithCompletionHandler:(RequestProductsCompletionHandler)completionHandler;
+- (void)loadProductsWithIdentifiers:(NSString *)firstIdentifier, ... ;
 
 @end
--- InAppPurchaseTools/SLKInAppProductManager.m
+++ InAppPurchaseTools/SLKInAppProductManager.m
@@ -27,19 +27,31 @@ NSSet * _productIdentifiers;
         
         // Store product identifiers
         _productIdentifiers = productIdentifiers;
-        
     }
     return self;
 }
 
-- (void)requestProductsWithCompletionHandler:(RequestProductsCompletionHandler)completionHandler {
+- (void)loadProductsWithIdentifiers:(NSString *)firstIdentifier, ...
+{
+    if(firstIdentifier == nil) {
+        return;
+    }
     
-    _completionHandler = [completionHandler copy];
-
+    NSMutableArray *identifiers = [[NSMutableArray alloc] init];
+    
+    va_list args;
+    va_start(args, firstIdentifier);
+    for (NSString *arg = firstIdentifier; arg != nil; arg = va_arg(args, NSString*))
+    {
+        [identifiers addObject:arg];
+    }
+    va_end(args);
+    
+    _productIdentifiers = [NSSet setWithArray:identifiers];
+    
     _productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:_productIdentifiers];
     _productsRequest.delegate = self;
     [_productsRequest start];
-    
 }
 
 #pragma mark - SKProductsRequestDelegate
@@ -58,21 +70,13 @@ NSSet * _productIdentifiers;
         [skProductIds addObject:skProduct.productIdentifier];
     }
     
-    NSDictionary *skProductDictionary = [NSDictionary dictionaryWithObjects:skProducts forKeys:skProductIds];
-    
-    _completionHandler(YES, skProductDictionary);
-    _completionHandler = nil;
-    
+    self.skProductDictionary = [NSDictionary dictionaryWithObjects:skProducts forKeys:skProductIds];
 }
 
 - (void)request:(SKRequest *)request didFailWithError:(NSError *)error {
     
     //NSLog(@"Failed to load list of products: %@", error);
     _productsRequest = nil;
-    
-    _completionHandler(NO, nil);
-    _completionHandler = nil;
-    
 }
 
 @end
