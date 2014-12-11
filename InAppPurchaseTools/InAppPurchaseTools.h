0c3e467f59a6ea8a898811895197f71c0aa4669f
Ben DiFrancesco <ben.difrancesco@gmail.com>
2014-12-11 12:06:12 -0500

    Initial Commit

---
+++ InAppPurchaseTools/InAppPurchaseTools.h
@@ -0,0 +1,19 @@
+//
+//  InAppPurchaseTools.h
+//  InAppPurchaseTools
+//
+//  Created by Ben Di on 12/11/14.
+//  Copyright (c) 2014 Scope Lift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppPurchaseTools.
+FOUNDATION_EXPORT double InAppPurchaseToolsVersionNumber;
+
+//! Project version string for InAppPurchaseTools.
+FOUNDATION_EXPORT const unsigned char InAppPurchaseToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppPurchaseTools/PublicHeader.h>
+
+
